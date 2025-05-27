d547f45e2b71f62cd4114d2c0ffdfe5f29154f39
Boreal <boreal@softmodd.ing>
2025-05-27 08:38:31 -0700

    fix: allow non bswappable type sizes in `BinaryReader::Read`

--- src/IO/BinaryIO.t.h
+++ src/IO/BinaryIO.t.h
@@ -15,8 +15,7 @@ namespace l4jf::io {
 	T BinaryReader::Read() {
 		// checks
 		static_assert(std::is_trivially_copyable_v<T>, "Needs a trivially copyable type");
-		static_assert( sizeof(T) != 2 || sizeof(T) != 4 || sizeof(T) != 8, "need 16, 32, or 64 bits for bswap" );
-		
+			
 		std::array<std::byte, sizeof(T)> array;
 		stream.read(reinterpret_cast<char*>(array.data()), sizeof(T));
 		
@@ -35,8 +34,7 @@ namespace l4jf::io {
 		std::array<std::byte, sizeof(T)> array;
 		std::memcpy(array.data(), &v, sizeof(T));
 
-		if (endian != SystemEndian)
-			std::reverse(array.begin(), array.end());
+		if (endian != SystemEndian) std::reverse(array.begin(), array.end());
 
 		stream.write(reinterpret_cast<char*>(array.data()), sizeof(T));
 	}
