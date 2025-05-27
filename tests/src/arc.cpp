3ff5c22797c66f0ce47538dbe7f8376e83e8d3b6
Boreal <boreal@softmodd.ing>
2025-05-26 19:13:41 -0700

    test: Added "Parse and Write ARC" test

---
+++ tests/src/arc.cpp
@@ -0,0 +1,20 @@
+#include <doctest/doctest.h>
+#include "Archive/ARCFile.h"
+#include <fstream>
+#include <sstream>
+
+TEST_CASE("Parse and write ARC") {
+    std::ifstream input("files/MediaWiiU.arc", std::ios::in | std::ios::binary);
+    CHECK(input.is_open());
+
+    std::stringstream original;
+    original << input.rdbuf();
+    input.clear();
+    input.seekg(0, std::ios::beg);
+
+    l4jf::arc::ARCFile arc(input);
+    std::ostringstream arcStream;
+    arc.Write(arcStream);
+
+    CHECK(arcStream.str() == original.str());
+}
