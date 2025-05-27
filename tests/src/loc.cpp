62871c3d3dbfe7a7d77ed113a3eebc5cc56c3056
Boreal <boreal@softmodd.ing>
2025-05-26 18:07:07 -0700

    tests: replace Catch2 with doctest
    I heard somewhere that doctest is faster than Catch2. Also that
    nohlmann::json makes use of it.

--- tests/src/loc.cpp
+++ tests/src/loc.cpp
@@ -1,22 +1,22 @@
-#include <catch2/catch_test_macros.hpp>
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
 #include "Localization/LOCFile.h"
 #include <fstream>
 #include <sstream>
 
 TEST_CASE("LOC <--> JSON") {
 	std::ifstream input("files/languages.loc", std::ios::in | std::ios::binary);
-	REQUIRE(input.is_open());
-	 
+	CHECK(input.is_open());
+
 	l4jf::loc::LOCFile loc(input);
-	
+
 	l4jf::loc::LOCFile locFromJson(*loc.ToJson());
-	
+
 	std::ostringstream locStream;
 	loc.Write(locStream);
-	
+
 	std::ostringstream locFromJsonStream;
 	locFromJson.Write(locFromJsonStream);
-	
-	REQUIRE(locStream.str() == locFromJsonStream.str());
-	
+
+	CHECK(locStream.str() == locFromJsonStream.str());
 }
